6c76913782c12f201969a125979a35d6b080eeb4
NITISH YADAV <101405119+thisisnitishyadav@users.noreply.github.com>
2025-04-08 15:21:03 +0530

    ny

---
+++ frontend/src/app/industry/[eduction]/page.js
@@ -0,0 +1,16 @@
+import Education from '@/components/industry/education /Education'
+import Footer from '@/layouts/footer/Footer'
+import Navbar from '@/layouts/navbar/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Education/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default page
